8997c08fce86927ade0480b5bcb78591b31c1dea
JS <JS@DESKTOP-LN77G2H>
2017-02-11 17:48:16 +0800

    bind GoICP by ICP.

--- main.cpp
+++ main.cpp
@@ -6,9 +6,19 @@
 #include <string>
 #include "opencv\cv.h"
 
-#include "GoICP/jly_goicp.h"
+#include "ICP.h"
+
 int main(int argc, char *argv[])
 {
+
+	STLloader model1("porsche.stl");
+	STLloader lower("lower.stl");
+	model1.load();
+	lower.load();
+	ICP icp1;
+	icp1.load_points(lower.get_vertexs(), lower.get_vertexs());
+	icp1.readConfig("config.txt");
+	icp1.run();
 
 	QApplication a(argc, argv);
 	MainWindow w;
